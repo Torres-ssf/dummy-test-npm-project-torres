78aa691aa68386a5d610cac47d9c06a9aea03364
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2023-04-23 16:18:26 -0300

    Revert "improve 2"

--- utility.js
+++ utility.js
@@ -2,16 +2,6 @@ function add (a, b) {
   return a + b;
 }
 
-function divide (a, b) {
-  return a / b;
-}
-
-function module (a, b) {
-  return a % b;
-}
-
 module.exports = {
   add,
-  divide,
-  module,
 };
